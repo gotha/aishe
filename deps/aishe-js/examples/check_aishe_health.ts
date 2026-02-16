902b202585222d0daa5f37e95d20e518620531cd
Pavel Pashov <pavel.pashov@redis.com>
2026-02-16 20:49:03 +0200

    chore: remove deps/aishe-js

--- deps/aishe-js/examples/check_aishe_health.ts
+++
@@ -1,7 +0,0 @@
-import { RAGAPIClient } from "../src/client.js";
-
-console.log("EXAMPLE: Checking AIshe's health... 🤖");
-
-const client = new RAGAPIClient();
-const health = await client.checkHealth();
-console.log("Health:", health);
